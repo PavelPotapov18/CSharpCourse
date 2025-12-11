ded40d216395c08122a762d9b4efab3b51b1ad6d
pavel.potapov@inproec.com <pavel.potapov@inproec.com>
2025-12-11 13:41:24 +0300

    newBranch

---
+++ App3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace App3._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+        Console.Write(result);
+    }
+}
---
+++ App3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace App3._2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        double result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result += 1 / (double)i;
+        }
+        Console.Write(result);
+    }
+}
